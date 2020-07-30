3b9ed762edc4a24886006ccc0fc1cdba06ff6cba
José Alexandro Uc Santos <alexandro.us@icloud.com>
2020-07-30 11:08:39 -0500

    docs agregado

---
+++ docs/precache-manifest.d01bf32ba0f4d7e14b4f725743afd0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21660da948f87ea8ba73f21c1f762b3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a2cd3c4c95dbd5905f5",
+    "url": "/static/css/main.80658bc0.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2cd3c4c95dbd5905f5",
+    "url": "/static/js/main.ff08e465.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
